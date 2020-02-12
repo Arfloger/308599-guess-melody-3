ec3fa1b2191d47c208f08a5942cad20077728ad0
ksenia <arfloger@gmail.com>
2020-02-12 21:58:27 +0300

    8. Учебный проект: под покровом тестов

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,10 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import WelcomeScreen from '../welcome-screen/welcome-screen.jsx';
 
+const welcomeButtonHandler = () => {};
+
 const App = (props) => {
   const {errorsCount} = props;
   return (
-    <WelcomeScreen errors={errorsCount}/>
+    <WelcomeScreen
+      errors={errorsCount}
+      onWelcomeButtonClick={welcomeButtonHandler}
+    />
   );
 };
 
